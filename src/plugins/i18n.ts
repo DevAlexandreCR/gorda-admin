49884db04827596295b0b4a698fc81076bb77902
Alexanderbyrush <alexandrecamilor@gmail.com>
2021-12-15 18:05:11 -0500

    security

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -7,10 +7,10 @@ import { createI18n, LocaleMessages, VueMessageType } from 'vue-i18n'
  * See: https://github.com/intlify/vue-i18n-loader#rocket-i18n-resource-pre-compilation
  */
 function loadLocaleMessages(): LocaleMessages<VueMessageType> {
-  const locales = require.context('../locales', true, /[A-z0-9-_,\s]+\.json$/i)
+  const locales = require.context('../locales', true, /[i]\.json$/i)
   const messages: LocaleMessages<VueMessageType> = {}
   locales.keys().forEach(key => {
-    const matched = key.match(/([A-z0-9-_]+)\./i)
+    const matched = key.match(/[i]\./i)
     if (matched && matched.length > 1) {
       const locale = matched[1]
       messages[locale] = locales(key).default
