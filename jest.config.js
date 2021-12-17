cc10c9341b103a07a29bccdfe386de583542e451
Alexander Camilo <fredy.camilo@evertecinc.com>
2021-12-16 22:15:46 -0500

    feature: add coverage

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{js,vue,ts}']
 }
