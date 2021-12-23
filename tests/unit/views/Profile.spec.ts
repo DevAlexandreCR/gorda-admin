3ebe00521d62aabfa07ec3d03069ac5b907dc3ef
Alexander Camilo <fredy.camilo@evertecinc.com>
2021-12-23 17:35:05 -0500

    test: add test to Profile.vue

---
+++ tests/unit/views/Profile.spec.ts
@@ -0,0 +1,29 @@
+import {mount, VueWrapper} from '@vue/test-utils'
+import router from '@/router'
+import i18n from '@/plugins/i18n'
+import AuthService from '@/services/AuthService'
+import User from '@/models/User'
+import UserInterface from '../../mocks/entities/UserInterface'
+import Profile from '@/views/Profile.vue'
+
+describe('Profile.vue', () => {
+  AuthService.currentUser = new User(UserInterface)
+  let wrapper: VueWrapper<any>
+  beforeEach(async () => {
+    wrapper = mount(Profile,
+      {
+        global: {
+          plugins: [router, i18n]
+        },
+      })
+    await router.isReady()
+  })
+
+  it('profile component show user information', async () => {
+    expect(wrapper.vm.user).toStrictEqual(AuthService.currentUser)
+    expect(wrapper.html()).toContain(AuthService.currentUser.name)
+    expect(wrapper.html()).toContain(AuthService.currentUser.email)
+    expect(wrapper.html()).toContain(AuthService.currentUser.phone)
+    expect(wrapper.html()).toContain('Administrator')
+  })
+})
