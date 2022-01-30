115a21d8a306ee4d7ab8e2e57c106253ef82fb5c
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-01-30 16:53:15 -0500

    refactor: add checkbox fields

--- src/views/users/Edit.vue
+++ src/views/users/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container-fluid pb-4" v-if="user">
+  <div class="container-fluid pb-4" v-if="user.id">
     <Form @submit="updateUser" :validation-schema="schema">
       <div class="card w-75 mx-auto">
         <div class="card-header text-center text-capitalize">
@@ -9,16 +9,14 @@
           <div class="row">
             <div class="col-md-5">
               <div class="card-header p-0 mx-3 mt-3 z-index-1">
-                <img src="../../assets/img/logo.png" class="img-fluid border-radius-lg" alt="profile_photo">
+                <img :src="this.user.photoUrl" class="img-fluid border-radius-lg" alt="profile_photo">
               </div>
             </div>
             <div class="col-md-7">
               <div class="form-group">
                 <label>{{ $t('users.fields.name') }}</label>
-                <div class="input-group">
                   <Field name="name" type="text" class="form-control" :placeholder=" $t('common.placeholders.name')" v-model="user.name" id="name" aria-label="Name" aria-describedby="name-addon" />
                     <ErrorMessage name="name"/>
-                </div>
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.email') }}</label>
@@ -27,25 +25,23 @@
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.phone') }}</label>
-                <div class="input-group">
                   <Field name="phone" type="phone" class="form-control" :placeholder=" $t('common.placeholders.phone')" id="phone" v-model="user.phone" aria-label="Phone" aria-describedby="phone-addon"/>
                     <ErrorMessage name="phone"/>
-                </div>
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.role') }}:</label>
                 <div class="form-check mb-4 d-inline-block ms-4">
-                  <Field class="form-check-input" type="checkbox" @change="assignRole" name="role" id="customRadio0" :value="user.roles?.operator"/>
+                  <input class="form-check-input" type="checkbox" @change="assignRole" name="role" id="customRadio0" :value="'operator'" :checked="user.roles.operator"/>
                   <label class="custom-control-label">{{ $t('users.fields.operator') }}</label>
                 </div>
                 <div class="form-check d-inline-block ms-3">
-                  <Field class="form-check-input" type="checkbox" name="role" @change="assignRole" id="customRadio1" :value="user.roles?.admin"/>
+                  <input class="form-check-input" type="checkbox" name="role" @change="assignRole" id="customRadio1" :value="'admin'" :checked="user.roles.admin"/>
                   <label class="custom-control-label">{{ $t('users.fields.admin') }}</label>
                 </div>
                 <ErrorMessage name="role"/>
               </div>
               <div class="form-check form-switch">
-                <Field class="form-check-input" name="enable" type="checkbox" id="flexSwitchCheckDefault" :checked="!!user.enabled_at" @change="onEnable"/>
+                <input class="form-check-input" name="enable" type="checkbox" id="flexSwitchCheckDefault" :checked="user.isEnabled()" @change="onEnable"/>
                 <label class="form-check-label">{{ $t(user.enabled_at ? 'common.fields.enabled' : 'common.fields.disabled') }}</label>
                 <ErrorMessage name="enable"/>
               </div>
@@ -79,14 +75,12 @@ import * as yup from 'yup'
 
 export default class Edit extends Vue {
   user: User = new User({})
-  admin: boolean = false
 
   readonly schema = yup.object().shape({
     name: yup.string().required().min(3),
     email: yup.string().required().email(),
     phone: yup.string().required().min(8),
-    role: yup.array(),
-    enabled_at: yup.string().required()
+    role: yup.array()
   })
 
   updateUser(): void {
@@ -100,36 +94,30 @@ export default class Edit extends Vue {
     }).catch(e => {
       Swal.fire({
         icon: 'error',
-        title: 'Oops...',
         title: this.$t('common.messages.error'),
         text: e.message,
       })
     })
   }
 
-  onEnable(): void {
-    this.user.enabled_at = this.user.isEnabled() ? null : new Date().toLocaleDateString()
+  onEnable(e: Event): void {
+    const target = e.target as HTMLInputElement
+    this.user.enabled_at = target.checked ? new Date().toLocaleDateString() : ''
   }
 
-  assignRole(): void {
-    console.log(this.user.roles)
-    // if (this.admin) {
-    //   this.user.roles = {
-    //     operator: true,
-    //     admin: false
-    //   }
-    // } else {
-    //   this.user.roles = {
-    //     operator: false,
-    //     admin: true
-    //   }
-    //   }
+  assignRole(e: Event): void {
+    const target = e.target as HTMLInputElement
+    if (target.value === 'operator') {
+      this.user.roles!.operator = target.checked
+    } else {
+      this.user.roles!.admin = target.checked
+      }
   }
 
   created(): void {
     UserRepository.getUser(this.$route.params.id as string).then(user => {
       this.user = new User(user)
-      this.admin = this.user.roles?.admin
+      this.user.photoUrl = this.user.photoUrl?? '../../assets/img/logo.png'
     })
   }
 }
