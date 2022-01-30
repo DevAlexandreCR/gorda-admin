e9a9ca25daea5a099fcef747bdbba9e74418c9f5
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-01-30 17:26:09 -0500

    refactor: add modal to img

--- src/views/users/Edit.vue
+++ src/views/users/Edit.vue
@@ -10,6 +10,9 @@
             <div class="col-md-5">
               <div class="card-header p-0 mx-3 mt-3 z-index-1">
                 <img :src="this.user.photoUrl" class="img-fluid border-radius-lg" alt="profile_photo">
+                <button class="btn btn-sm btn-icon btn-2 btn-primary btn-edit-img" type="button" data-bs-toggle="modal" data-bs-target="#imgModal">
+                  <span class="btn-inner--icon"><em class="fas fa-pen"></em></span>
+                </button>
               </div>
             </div>
             <div class="col-md-7">
@@ -54,6 +57,29 @@
         </div>
       </div>
     </Form>
+    <!-- Modal -->
+    <div class="modal fade" id="imgModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <div class="mb-3">
+              <label class="form-label">Default file input example</label>
+              <input class="form-control" type="file" id="formFile" accept="image/*"/>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn bg-gradient-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
