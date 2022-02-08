ff06994d3ec50cd5fb0deec8eeeec2e473536e6e
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-02-07 20:08:55 -0500

    ref: resolve codesmell

--- src/views/users/Index.vue
+++ src/views/users/Index.vue
@@ -7,6 +7,7 @@
       <div class="card-body px-0 pt-0 pb-2">
         <div class="table-responsive p-0">
           <table class="table align-items-center mb-0">
+            <caption hidden></caption>
             <thead>
             <tr>
               <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{ $t('users.fields.name') }}</th>
