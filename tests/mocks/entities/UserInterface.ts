d927239082aed0c5a92d9687002d232f872851ba
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-02-08 20:32:17 -0500

    test: add test to edit

--- tests/mocks/entities/UserInterface.ts
+++ tests/mocks/entities/UserInterface.ts
@@ -6,6 +6,8 @@ class UserInterface {
   email = 'admin@admin.com'
   phone = '310374656'
   photoUrl = 'https://fakeUrl.com'
+  created_at: '2020-01-01'
+  enabled_at: '2022-01-01'
   roles: Roles =  {
     admin: true,
     operator: false
--- tests/unit/views/users/Edit.spec.ts
+++ tests/unit/views/users/Edit.spec.ts
@@ -7,15 +7,23 @@ import UserInterface from '../../../mocks/entities/UserInterface'
 import UserRepository from '@/repositories/UserRepository'
 import {ErrorMessage, Field, Form} from 'vee-validate'
 import Edit from '@/views/users/Edit.vue'
+import {flushPromises} from '@vue/test-utils'
+import waitForExpect from 'wait-for-expect'
+import Swal from 'sweetalert2'
 
 UserRepository.getUser = jest.fn().mockResolvedValue(UserInterface)
+UserRepository.update = jest.fn().mockResolvedValue(UserInterface)
 
 describe('Edit.vue', () => {
   AuthService.currentUser = new User(UserInterface)
   let wrapper: VueWrapper<any>
+  const div = document.createElement('div')
+  div.id = 'root'
+  document.body.appendChild(div)
   beforeEach(async () => {
     wrapper = mount(Edit,
       {
+        attachTo: '#root',
         components: {
           Form,
           Field,
@@ -54,10 +62,55 @@ describe('Edit.vue', () => {
     const inputPhone = wrapper.find('#phone')
     await inputPhone.setValue(updatedUser.phone)
     const button =  wrapper.find('button[type="submit"]')
-    await button.trigger('submit')
+    await button.trigger('click')
 
-    expect(wrapper.vm.user.name).toBe(updatedUser.name)
-    expect(wrapper.vm.user.email).toBe(updatedUser.email)
-    expect(wrapper.vm.user.phone).toBe(updatedUser.phone)
+    await flushPromises()
+    await waitForExpect(() => {
+      expect(wrapper.vm.user.name).toBe(updatedUser.name)
+      expect(wrapper.vm.user.email).toBe(updatedUser.email)
+      expect(wrapper.vm.user.phone).toBe(updatedUser.phone)
+
+    });
+  })
+
+  it('show error when update function fails', async () => {
+    const swal = spyOn(Swal,'fire')
+    UserRepository.update = jest.fn().mockRejectedValue(new Error('New Error'))
+    const button =  wrapper.find('button[type="submit"]')
+    await button.trigger('click')
+
+    await flushPromises()
+    await waitForExpect(() => {
+      expect(swal).toBeCalledWith({
+        icon: 'error',
+        title: wrapper.vm.$t('common.messages.error'),
+        text: 'New Error',
+      })
+    });
+  })
+
+  it('user can enable or disable user', async () => {
+    const enable =  wrapper.find('input[name="enable"]')
+    await enable.trigger('click')
+    expect(wrapper.vm.user.enabled_at).toBe(new Date().toLocaleDateString())
+    await enable.trigger('click')
+    expect(wrapper.vm.user.enabled_at).toBe('')
+  })
+
+  it('user can assign role to user', async () => {
+    const operator =  wrapper.find('#operator')
+    await operator.trigger('click')
+    const admin =  wrapper.find('#admin')
+    await admin.trigger('click')
+
+    expect(wrapper.vm.user.roles.operator).toBeTruthy()
+    expect(wrapper.vm.user.roles.admin).toBeFalsy()
+  })
+
+  it('an user can edit urlPhoto', async () => {
+    await wrapper.vm.$nextTick()
+    const updateUser = spyOn(wrapper.vm,'updateUser')
+    wrapper.vm.uploadImg()
+    expect(updateUser).toBeCalled()
   })
 })
