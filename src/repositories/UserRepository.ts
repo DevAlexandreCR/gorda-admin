0090c7bed17e2547e69477eb68ee03c19ca60b93
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-02-09 19:32:23 -0500

    test: exclude coverage

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -4,16 +4,19 @@ import {UserInterface} from '@/entities/UserInterface'
 
 class UserRepository{
 
+  /* istanbul ignore next */
   async getUser(id: string): Promise<UserInterface> {
     const snapshot: DataSnapshot = await get(child(DBService.dbUsers(), id))
     return <UserInterface>snapshot.val()
   }
 
+  /* istanbul ignore next */
   async getAll(): Promise<Array<UserInterface>> {
     const snapshot: DataSnapshot = await get(DBService.dbUsers())
     return <Array<UserInterface>>snapshot.val()
   }
 
+  /* istanbul ignore next */
   update(user: UserInterface): Promise<void> {
     return set(ref(DBService.db, 'users/' + user.id), user);
   }
