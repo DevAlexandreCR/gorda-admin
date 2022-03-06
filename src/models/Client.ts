d56bce7189d4a7ca47c2d9bbb4c01ff77c5da760
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-03-06 18:35:02 -0500

    refactor: implement Interface

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,17 +1,9 @@
 import {ClientInterface} from '@/entities/ClientInterface'
 
-export default class Client {
+export default class Client implements ClientInterface {
   id: string
   name: string
   email: string|null
   phone: string
-  created_at?: string
-
-  constructor(client: ClientInterface) {
-    this.id = client.id
-    this.name = client.name
-    this.email = client.email?? ''
-    this.phone = client.phone
-    this.created_at = client.created_at
-  }
+  created_at: number
 }
