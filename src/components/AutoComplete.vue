8c6a98e1d040ed637b6784e8bff878ff6f6e8296
Alexander Camilo <fredy.camilo@evertecinc.com>
2022-03-12 18:58:01 -0500

    ref: rename vars

--- src/components/AutoComplete.vue
+++ src/components/AutoComplete.vue
@@ -4,16 +4,16 @@
                
       <div class="form-group">
             <div class="input-group">
-              <Field  type="text" id="search" :name="fieldName" v-model="searchTerm" @keyup="searchCountry" placeholder="Type here..." class="form-control "/>
+              <Field type="text" id="search" :name="fieldName" v-model="searchElement" @keyup="searchCountry" placeholder="Type here..." class="form-control "/>
             </div>
          </div>
 
       <ul v-if="searchElements.length > 1" class="w-full rounded text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 ">
         <li class="px-1 pt-1 pb-2 font-bold border-b border-gray-200">
-          Showing {{ searchElements.length }} of {{ Elements.length }} results
+          Showing {{ searchElements.length }} of {{ elements.length }} results
         </li>
-        <li v-for="country in searchElements" :key="country" @click="selectCountry(country)" class="cursor-pointer hover:bg-gray-100 p-1">
-          {{ country }}
+        <li v-for="element in searchElements" :key="element" @click="selectElement(element)" class="cursor-pointer hover:bg-gray-100 p-1">
+          {{ element }}
         </li>
       </ul>
     </div>
@@ -26,7 +26,7 @@ import {Options, Vue} from 'vue-class-component'
 import {Field} from 'vee-validate'
 
 class Props {
-  Elements: Array<any>
+  elements: Array<any>
   fieldName: string
 }
 
@@ -37,29 +37,29 @@ class Props {
 })
 export default class AutoComplete  extends Vue.with(Props){
 
-  selectedCountry = ''
+  selectedElement = ''
   searchElements: Array<any> = [] 
-  searchTerm = ''
+  searchElement = ''
 
-  // mounted(){
-    //console.log(this.Elements);
-  //}
+  mounted(){
+    console.log(this.elements);
+  }
 
   searchCountry(): void {
     let matches = 0
-    if (this.searchTerm.length > 2) {
-      this.searchElements = this.Elements.filter(country => {
-        if (country.toLowerCase().includes(this.searchTerm.toLowerCase()) && matches < 5) {
+    if (this.searchElement.length > 2) {
+      this.searchElements = this.elements.filter(element => {
+        if (element.toLowerCase().includes(this.searchElement.toLowerCase()) && matches < 5) {
           matches++
-          return country
+          return element
         }
       })
     }
   }
 
-  selectCountry(country: string): void {
-    this.selectedCountry = country
-    this.searchTerm = country
+  selectElement(element: string): void {
+    this.selectedElement = element
+    this.searchElement = element
   }
 }
 </script>
