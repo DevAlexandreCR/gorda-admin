97ef2ef43f8d0d6f7f02440277fdda99ae28f3a2
AlexandreCR <devalexandrecr@gmail.com>
2022-05-04 20:37:44 -0500

    ref: config eslint

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
+module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
-  ],
-  lintOnSave: false
-})
+  ]
+}
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,6 @@ module.exports = {
       compositionOnly: true,
       fullInstall: true
     }
-  }
+  },
+  lintOnSave: false
 }
