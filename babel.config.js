fe4872c3b580ec0a6bdf22fc1d61a94359ee8a4f
AlexandreCR <devalexandrecr@gmail.com>
2022-05-04 20:34:27 -0500

    ref: config eslint

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,7 @@
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
   presets: [
     '@vue/cli-plugin-babel/preset',
-  ]
-}
+  ],
+  lintOnSave: false
+})
