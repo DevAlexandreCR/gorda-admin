fe79e9440393ef8a5d18c221617dc3ad2882a4b1
AlexandreCR <devalexandrecr@gmail.com>
2022-05-06 21:31:11 -0500

    test: ref ImageLoader.spec.ts

--- src/components/ImageLoader.vue
+++ src/components/ImageLoader.vue
@@ -63,7 +63,7 @@ export default class ImageLoader extends Vue.with(Props) {
     const ref = StorageService.getStorageReference(this.path, this.resourceId, this.image[0]?.name)
     StorageService.uploadFile(ref, this.image[0]).then(url => {
       this.$emit(this.event, url)
-      const modal = document.getElementById(this.id) as Element
+      const modal = document.getElementById(this.id) as HTMLElement
       const modalImg = new Modal(modal)
       modalImg?.hide()
     })
