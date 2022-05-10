06e5283f34c93f292214a759c2bf4c497fffdf69
AlexandreCR <devalexandrecr@gmail.com>
2022-05-09 20:32:58 -0500

    refactor: remove unused Class

--- src/models/Client.ts
+++
@@ -1,9 +0,0 @@
-import {ClientInterface} from '@/types/ClientInterface'
-
-export default class Client implements ClientInterface {
-  id: string
-  name: string
-  email: string|null
-  phone: string
-  created_at: number
-}
--- src/types/ClientInterface.ts
+++
@@ -1,8 +0,0 @@
-
-export interface ClientInterface {
-  id: string
-  name: string
-  phone: string
-  email: string|null
-  created_at: number
-}
