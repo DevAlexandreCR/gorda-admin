1df286992010219b63f9918f6ec8f6b4ad3471b5
AlexandreCR <devalexandrecr@gmail.com>
2022-05-09 20:27:01 -0500

    test: complete test to AssignDriver.spec.ts

--- tests/unit/components/services/AssignDriver.spec.ts
+++ tests/unit/components/services/AssignDriver.spec.ts
@@ -1,4 +1,4 @@
-import {mount, VueWrapper} from '@vue/test-utils'
+import {flushPromises, mount, VueWrapper} from '@vue/test-utils'
 import router from '@/router'
 import i18n from '@/plugins/i18n'
 import AssignDriver from '@/components/services/AssingDriver.vue'
@@ -7,7 +7,7 @@ import {nextTick, render} from 'vue'
 import AutoComplete from '@/components/AutoComplete.vue'
 import Driver from '@/models/Driver'
 import Swal from 'sweetalert2'
-import waitForExpect from 'wait-for-expect'
+import ServiceRepository from '@/repositories/ServiceRepository'
 
 describe('AssignDriver.vue', () => {
   let wrapper: VueWrapper<any>
@@ -41,7 +41,6 @@ describe('AssignDriver.vue', () => {
     expect(wrapper.findComponent(AutoComplete).exists()).toBeTruthy()
     expect(wrapper.find('.btn-secondary').text()).toMatch(wrapper.vm.$t('common.actions.close'))
     expect(wrapper.find('.btn-primary').text()).toMatch(wrapper.vm.$t('common.actions.assign'))
-    wrapper.unmount()
   })
   
   it('should fill plates with river given', async () => {
@@ -74,6 +73,49 @@ describe('AssignDriver.vue', () => {
       title: wrapper.vm.$t('common.messages.updated'),
       showConfirmButton: false,
       text: undefined,
+      timer: 1500,
+      toast:true
+    })
+  })
+  
+  it('should show error toast when click on assign without driver selected', async () => {
+    const swal = jest.spyOn(Swal,'fire')
+    await nextTick()
+    await wrapper.find('.btn-primary').trigger('click')
+    
+    expect(swal).toBeCalledWith({
+      icon: 'error',
+      position: 'top-right',
+      title: wrapper.vm.$t('common.messages.error'),
+      showConfirmButton: false,
+      text: wrapper.vm.$t('validations.driver'),
+      timer: 1500,
+      toast:true
+    })
+  })
+  
+  it('should show error toast when something was wrong while try to update', async () => {
+    const swal = jest.spyOn(Swal,'fire')
+    ServiceRepository.update = jest.fn().mockRejectedValue(new Error('something was wrong'))
+    
+    await nextTick()
+    await button.click()
+    const input = wrapper.find('input')
+    await input.setValue('HEM')
+    await input.trigger('keyup', {
+      keyCode: 72
+    })
+    await nextTick()
+    await wrapper.find('li').trigger('click')
+    await wrapper.find('.btn-primary').trigger('click')
+    await flushPromises()
+    
+    expect(swal).toBeCalledWith({
+      icon: 'error',
+      position: 'top-right',
+      title: wrapper.vm.$t('common.messages.error'),
+      showConfirmButton: false,
+      text: 'something was wrong',
       timer: 1500,
       toast:true
     })
