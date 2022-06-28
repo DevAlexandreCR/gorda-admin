69296831b09148726b6e1e19232b3966b52a8b2d
AlexandreCR <devalexandrecr@gmail.com>
2022-06-27 21:02:51 -0500

    feature: enable/disable users

--- src/views/users/Edit.vue
+++ src/views/users/Edit.vue
@@ -10,7 +10,8 @@
             <div class="col-md-5">
               <div class="card-header p-0 mx-3 mt-3 z-index-1">
                 <img :src="this.user.photoUrl" class="img-fluid border-radius-lg" alt="profile_photo">
-                <button class="btn btn-sm btn-icon btn-2 btn-primary btn-edit-img" type="button" data-bs-toggle="modal" data-bs-target="#imgModal">
+                <button class="btn btn-sm btn-icon btn-2 btn-primary btn-edit-img" type="button" data-bs-toggle="modal"
+                        data-bs-target="#imgModal">
                   <span class="btn-inner--icon"><em class="fas fa-pen"></em></span>
                 </button>
               </div>
@@ -18,41 +19,49 @@
             <div class="col-md-7">
               <div class="form-group">
                 <label>{{ $t('users.fields.name') }}</label>
-                  <Field name="name" type="text" v-slot="{ field, errorMessage, meta }" v-model="user.name">
-                      <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.name')" id="name" aria-label="Name" aria-describedby="name-addon" v-bind="field" />
-                      <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
-                  </Field>
+                <Field name="name" type="text" v-slot="{ field, errorMessage, meta }" v-model="user.name">
+                  <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.name')"
+                         id="name" aria-label="Name" aria-describedby="name-addon" v-bind="field"/>
+                  <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
+                </Field>
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.email') }}</label>
                 <Field name="email" type="email" v-slot="{ field, errorMessage, meta }" v-model="user.email">
-                      <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.email')" id="email" aria-label="Email" aria-describedby="email-addon" v-bind="field" />
-                      <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
-                  
+                  <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.email')"
+                         id="email" aria-label="Email" aria-describedby="email-addon" v-bind="field"/>
+                  <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
+
                 </Field>
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.phone') }}</label>
-                  <Field name="phone" type="phone"  v-slot="{ field, errorMessage, meta }" v-model="user.phone">
-                    <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.phone')"  id="phone" aria-label="Phone" aria-describedby="phone-addon" v-bind="field" />
-                    <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
-                  </Field>
+                <Field name="phone" type="phone" v-slot="{ field, errorMessage, meta }" v-model="user.phone">
+                  <input class="form-control" v-model="field.value" :placeholder="$t('common.placeholders.phone')"
+                         id="phone" aria-label="Phone" aria-describedby="phone-addon" v-bind="field"/>
+                  <span class="is-invalid" v-if="errorMessage || !meta.dirty">{{ errorMessage }}</span>
+                </Field>
               </div>
               <div class="form-group">
                 <label>{{ $t('users.fields.role') }}:</label>
                 <div class="form-check mb-4 d-inline-block ms-4">
-                  <input class="form-check-input" type="checkbox" @change="assignRole" name="role" id="operator" :value="'operator'" :checked="user.roles.operator"/>
+                  <input class="form-check-input" type="checkbox" @change="assignRole" name="role" id="operator"
+                         :value="'operator'" :checked="user.roles.operator"/>
                   <label class="custom-control-label">{{ $t('users.fields.operator') }}</label>
                 </div>
                 <div class="form-check d-inline-block ms-3">
-                  <input class="form-check-input" type="checkbox" name="role" @change="assignRole" id="admin" :value="'admin'" :checked="user.roles.admin"/>
+                  <input class="form-check-input" type="checkbox" name="role" @change="assignRole" id="admin"
+                         :value="'admin'" :checked="user.roles.admin"/>
                   <label class="custom-control-label">{{ $t('users.fields.admin') }}</label>
                 </div>
                 <ErrorMessage name="role"/>
               </div>
               <div class="form-check form-switch">
-                <input class="form-check-input" name="enable" type="checkbox" id="flexSwitchCheckDefault" :checked="user.isEnabled()" @change="onEnable"/>
-                <label class="form-check-label">{{ $t(user.enabled_at ? 'common.fields.enabled' : 'common.fields.disabled') }}</label>
+                <input class="form-check-input" name="enable" type="checkbox" id="flexSwitchCheckDefault"
+                       :checked="user.isEnabled()" @change="onEnable"/>
+                <label class="form-check-label">{{
+                    $t(user.enabled_at ? 'common.fields.enabled' : 'common.fields.disabled')
+                  }}</label>
                 <ErrorMessage name="enable"/>
               </div>
             </div>
@@ -65,7 +74,8 @@
       </div>
     </Form>
     <!-- Modal -->
-    <div class="modal fade" id="imgModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="imgModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+         aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
           <div class="modal-header">
@@ -83,7 +93,9 @@
               </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">{{ $t('common.actions.close') }}</button>
+              <button type="button" class="btn bg-gradient-secondary" data-bs-dismiss="modal">
+                {{ $t('common.actions.close') }}
+              </button>
               <button type="submit" class="btn bg-gradient-primary">{{ $t('common.actions.submit') }}</button>
             </div>
           </Form>
@@ -93,93 +105,92 @@
   </div>
 </template>
 
-<script lang="ts">
-import {Options, Vue} from 'vue-class-component'
+<script setup lang="ts">
 import UserRepository from '@/repositories/UserRepository'
 import User from '@/models/User'
 import {ErrorMessage, Field, Form} from 'vee-validate'
 import Swal from 'sweetalert2'
 import * as yup from 'yup'
+import {ObjectSchema} from 'yup'
 import CustomValidator from '@/assets/validatiions/validators'
 import * as bootstrap from 'bootstrap'
 import StorageService from '@/services/StorageService'
 import dayjs from 'dayjs'
+import DriverRepository from '@/repositories/DriverRepository'
+import i18n from '@/plugins/i18n'
+import ToastService from '@/services/ToastService'
+import {onBeforeMount, ref, Ref} from 'vue'
+import {useRoute} from 'vue-router'
 
-@Options({
-  components: {
-    Form,
-    Field,
-    ErrorMessage
-  },
+const user: Ref<User> = ref(new User)
+const route = useRoute()
+const schemaImg: ObjectSchema<any> = yup.object().shape({
+  photo: CustomValidator.isImage(i18n.global.t('validations.image'), i18n.global.t('validations.size')).required()
+})
+const image: Ref<File[]> = ref([])
+
+const schema = yup.object().shape({
+  name: yup.string().required().min(3),
+  email: yup.string().required().email(),
+  phone: yup.string().required().min(8),
+  role: yup.array()
 })
 
-export default class Edit extends Vue {
-  user: User = new User()
-  schemaImg: yup.ObjectSchema<any>
-  image: File[] = []
-
-  readonly schema = yup.object().shape({
-    name: yup.string().required().min(3),
-    email: yup.string().required().email(),
-    phone: yup.string().required().min(8),
-    role: yup.array()
+function uploadImg(): void {
+  const ref = StorageService.getStorageReference(StorageService.profilePath, user.value.id ?? '', image.value[0]?.name)
+  StorageService.uploadFile(ref, image.value[0]).then(url => {
+    user.value.photoUrl = url
+    updateUser()
   })
+}
 
-  mounted(): void {
-    this.schemaImg = yup.object().shape({
-      photo: CustomValidator.isImage(this.$t('validations.image'), this.$t('validations.size')).required()
+function updateUser(): void {
+  UserRepository.update(user.value).then(() => {
+    Swal.fire({
+      icon: 'success',
+      title: i18n.global.t('common.messages.updated'),
+      showConfirmButton: false,
+      timer: 1500
     })
-  }
-
-  uploadImg(): void {
-    const ref = StorageService.getStorageReference(StorageService.profilePath, this.user.id?? '', this.image[0]?.name)
-    StorageService.uploadFile(ref, this.image[0]).then(url => {
-      this.user.photoUrl = url
-      this.updateUser()
+    const modal = document.getElementById('imgModal')
+    const modalImg = bootstrap.Modal.getOrCreateInstance(modal ?? '')
+    modalImg.hide()
+  }).catch(e => {
+    Swal.fire({
+      icon: 'error',
+      title: i18n.global.t('common.messages.error'),
+      text: e.message
     })
-  }
+  })
+}
 
-  updateUser(): void {
-    UserRepository.update(this.user).then(() => {
-      Swal.fire({
-        icon: 'success',
-        title: this.$t('common.messages.updated'),
-        showConfirmButton: false,
-        timer: 1500
-      })
-      const modal = document.getElementById('imgModal')
-      const modalImg = bootstrap.Modal.getOrCreateInstance(modal?? '')
-      modalImg.hide()
-    }).catch(e => {
-      Swal.fire({
-        icon: 'error',
-        title: this.$t('common.messages.error'),
-        text: e.message,
-      })
-    })
-  }
+function onEnable(e: Event): void {
+  const target = e.target as HTMLInputElement
+  user.value.enabled_at = target.checked ? dayjs().unix() : 0
+  DriverRepository.enable(user.value.id ?? '', user.value.enabled_at).then(() => {
+    const message = user.value.enabled_at == 0 ?
+        i18n.global.t('users.messages.disabled') : i18n.global.t('users.messages.enabled')
+    ToastService.toast(ToastService.SUCCESS, message)
+  }).catch(e => {
+    ToastService.toast(ToastService.ERROR, i18n.global.t('common.messages.error'), e.message)
+  })
+}
 
-  onEnable(e: Event): void {
-    const target = e.target as HTMLInputElement
-    this.user.enabled_at = target.checked ? dayjs().unix() : null
-  }
-
-  assignRole(e: Event): void {
-    const target = e.target as HTMLInputElement
-    if (this.user.roles) {
-      if (target.value === 'operator') {
-        this.user.roles.operator = target.checked
-      } else {
-        this.user.roles.admin = target.checked
-      }
+function assignRole(e: Event): void {
+  const target = e.target as HTMLInputElement
+  if (user.value.roles) {
+    if (target.value === 'operator') {
+      user.value.roles.operator = target.checked
+    } else {
+      user.value.roles.admin = target.checked
     }
   }
-
-  created(): void {
-    UserRepository.getUser(this.$route.params.id as string).then(user => {
-      Object.assign(this.user, user)
-      this.user.photoUrl = this.user.photoUrl?? '../../assets/img/logo.png'
-    })
-  }
 }
+
+onBeforeMount(() => {
+  UserRepository.getUser(route.params.id as string).then(userDB => {
+    Object.assign(user.value, userDB)
+    user.value.photoUrl = user.value.photoUrl ?? '../../assets/img/logo.png'
+  })
+})
 </script>
