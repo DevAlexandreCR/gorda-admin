5922cdb7b0b1e7fac68a5e9a4918f8ad6116ced5
AlexandreCR <devalexandrecr@gmail.com>
2022-07-04 15:11:10 -0500

    refactor: fix api key

--- firebase.config.js
+++ firebase.config.js
@@ -1,10 +1,10 @@
 export default {
-    apiKey: process.env.VUE_APP_API_KEY ?? "AIzaSyBEF9nEWRooJ8Ov5mQW0tUtBp95sn5y2E4",
+    apiKey: process.env.VUE_APP_API_KEY ?? "",
     authDomain: process.env.VUE_APP_AUTHDOMAIN ?? "gorda.local",
     projectId: process.env.VUE_APP_PROJECT_ID ?? "gorda-local",
     storageBucket: process.env.VUE_APP_STORAGE_BUCKET ?? "gorda-local",
     messagingSenderId: process.env.VUE_APP_MESSAGING_ID ?? "12345678900",
     appId: process.env.VUE_APP_APP_ID ?? "1:412940684394:web:40f1a71786e91a53040e13",
-    measurementId: process.env.VUE_APP_MEASUREMENT_ID ?? "G-7P3L39LYYR"
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID ?? "G-858886884"
 }
 
