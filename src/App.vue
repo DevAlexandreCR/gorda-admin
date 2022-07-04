5426695afff26a0ccf1c211c5e9df35b394db05a
AlexandreCR <devalexandrecr@gmail.com>
2022-07-04 12:00:00 -0500

    refactor: add storage

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,19 @@ import {onMounted} from 'vue'
 import {usePlacesStore} from '@/services/stores/PlacesStore'
 import {useClientsStore} from '@/services/stores/ClientsStore'
 import {useDriversStore} from '@/services/stores/DriversStore'
+import {useStorage} from '@/services/stores/Storage'
 
 const {getPlaces} = usePlacesStore()
 const {getClients} = useClientsStore()
 const {getDrivers} = useDriversStore()
+const {getLogoUrl} = useStorage()
 
 onMounted((): void => {
   AuthService.onAuthStateChanged(location.pathname)
   getPlaces()
   getClients()
   getDrivers()
+  getLogoUrl()
 })
 </script>
 
