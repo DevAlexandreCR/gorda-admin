82383cc59beec478d1bdb089c23f58acbcfe81e8
AlexandreCR <devalexandrecr@gmail.com>
2022-07-13 20:56:26 -0500

    ref: move stores initialization

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,8 +12,21 @@
 import SideBar from '@/components/SideBar.vue'
 import NavBar from '@/components/NavBar.vue'
 import {onMounted} from 'vue'
+import {usePlacesStore} from '@/services/stores/PlacesStore'
+import {useClientsStore} from '@/services/stores/ClientsStore'
+import {useDriversStore} from '@/services/stores/DriversStore'
+import {useStorage} from '@/services/stores/Storage'
+
+const {getPlaces} = usePlacesStore()
+const {getClients} = useClientsStore()
+const {getDrivers} = useDriversStore()
+const {getLogoUrl} = useStorage()
 
 onMounted((): void => {
   require('@/vendor/js/soft-ui-dashboard')
+  getPlaces()
+  getClients()
+  getDrivers()
+  getLogoUrl()
 })
 </script>
