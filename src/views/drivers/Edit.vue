2358ace71e894907473f3328e2efdee09f61159d
Alexanderbyrush <alexandrecamilor@gmail.com>
2022-11-19 20:40:13 -0500

    style: input css

--- src/views/drivers/Edit.vue
+++ src/views/drivers/Edit.vue
@@ -32,10 +32,10 @@
                 <div class="input-group">
                   <input type="email" class="form-control form-control-sm" :placeholder="$t('common.placeholders.email')" readonly
                     aria-label="Email" name="email" aria-describedby="email-addon" v-model="driver.email">
-                  <button class="btn btn-outline-secondary btn-sm" type="button" data-bs-toggle="modal" data-bs-target="#editGmail">
+                  <button class="btn-sm btn-outline-secondary" type="button" data-bs-toggle="modal" data-bs-target="#editGmail">
                     {{ $t('common.actions.edit') }}
                   </button>
-                </div>
+                  </div>
               </div>            
               <div class="form-group">
                 <label>{{ $t('users.fields.phone') }}</label>
