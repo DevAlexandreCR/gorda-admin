ed4421a76c8486829814125a43f44393eac4d8c6
AlexandreCR <fredy.camilo@evertecinc.com>
2023-02-13 21:15:50 -0500

    test: refactor removed button reset

--- tests/unit/views/Connection.spec.ts
+++ tests/unit/views/Connection.spec.ts
@@ -38,7 +38,6 @@ describe('Connection.vue', () => {
   test('it must throw event when click buttons', async () => {
     wrapper.vm.connected = true
     
-    await wrapper.find('.btn-danger').trigger('click')
     expect(wrapper.vm.qr).toBeNull()
     
     wrapper.vm.connected = false
