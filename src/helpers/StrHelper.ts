c59b288028645881b160caf58637160c97b1b1c6
AlexandreCR <fredy.camilo@evertecinc.com>
2023-02-22 22:58:02 -0500

    feature: format driver and service

--- src/helpers/StrHelper.ts
+++ src/helpers/StrHelper.ts
@@ -1,6 +1,7 @@
 export class StrHelper {
 	
 	static toCamelCase(str: string): string {
+		str = str.toLowerCase()
 		const arr = str.split(' ')
 		for (let i = 0; i < arr.length; i++) {
 			arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
@@ -12,5 +13,12 @@ export class StrHelper {
 			.replace(' ', '')
 			.replace(/[^\d]/g, "")
 			.slice(-10)
+	}
+	
+	static formatPlate(plate: string): string {
+		return plate
+			.toUpperCase()
+			.replace(' ', '')
+			.trim()
 	}
 }
