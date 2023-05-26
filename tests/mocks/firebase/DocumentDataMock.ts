a49e7f13f34e8b4afa12d42025ab9627710b7e55
AlexandreCR <fredy.camilo@evertecinc.com>
2023-05-25 21:40:22 -0500

    test: add mock

---
+++ tests/mocks/firebase/DocumentDataMock.ts
@@ -0,0 +1,12 @@
+import {DocumentData} from 'firebase/firestore'
+import ServiceMock from '../entities/ServiceMock'
+
+class DocumentDataMock implements DocumentData {
+	data = () => {
+		return {
+			ServiceMock
+		}
+	}
+}
+
+export default new DocumentDataMock()
