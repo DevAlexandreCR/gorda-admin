57ad65eb759be4656cdceba39d714309f91a66d0
AlexandreCR <fredy.camilo@evertecinc.com>
2023-08-16 21:50:42 -0500

    chore: update CHANGELOG.md

---
+++ tests/unit/views/metrics/Metrics.spec.ts
@@ -0,0 +1,55 @@
+import {flushPromises, mount, VueWrapper} from '@vue/test-utils'
+import router from '@/router'
+import i18n from '@/plugins/i18n'
+import DriverRepository from '@/repositories/DriverRepository';
+import DriverMock from '../../../mocks/entities/DriverMock';
+import {nextTick} from 'vue'
+import Metrics from '@/views/metrics/Metrics.vue'
+import {useMetricsStore} from '@/services/stores/MetricsStore'
+import axios from 'axios'
+import {ServiceStatus} from '@/types/ServiceStatus'
+import waitForExpect from 'wait-for-expect'
+
+axios.get = jest.fn().mockResolvedValue({
+	data: {
+		data: [
+			{
+				count: 30,
+				date: '2023-01-01',
+				type: 0,
+				status: ServiceStatus.Terminated
+			},
+			{
+				count: 15,
+				date: '2023-01-01',
+				type: 0,
+				status: ServiceStatus.Canceled
+			}
+		]
+	}
+})
+
+describe('Metrics.vue', () => {
+	let wrapper: VueWrapper<any>
+	beforeEach(async () => {
+		const metricsStore = useMetricsStore()
+		wrapper = mount(Metrics,
+			{
+				global: {
+					plugins: [router, i18n],
+					provide: {
+						'appName': 'test'
+					}
+				},
+			})
+		await router.isReady()
+	})
+	it('an user can show metrics', async () => {
+		await nextTick()
+		await flushPromises()
+		await waitForExpect(() => {
+			expect(wrapper.find('#my-chart-id').exists()).toBeTruthy()
+			expect(wrapper.find('#percent-chart').exists()).toBeTruthy()
+		})
+	})
+})
