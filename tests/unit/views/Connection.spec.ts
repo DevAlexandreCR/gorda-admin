a798bd65c0c02b4b2070e3bf3f97d094448d211a
alexandrecr <devalexandrecr@gmail.com>
2023-12-10 20:34:26 -0500

    test: update test CreateService.spec.ts

--- tests/unit/views/Connection.spec.ts
+++ tests/unit/views/Connection.spec.ts
@@ -6,24 +6,29 @@ import {openServer, socket} from '../../testSetup'
 import waitForExpect from 'wait-for-expect'
 import {WhatsApp} from '@/services/gordaApi/constants/WhatsApp'
 import {useWpClientsStore} from "@/services/stores/WpClientStore";
+import {nextTick} from "vue";
+import {WpClient} from "@/types/WpClient";
 
 describe('Connection.vue', () => {
   let wrapper: VueWrapper<any>
 
-  
   beforeEach(async () => {
     const wpClient = useWpClientsStore()
+    const client: WpClient = {
+      id: '3103794656',
+      alias: 'Principal',
+      wpNotifications: false,
+      chatBot: false
+    }
     wpClient.clients = {
-      3103794656: {
-        id: '3103794656',
-        alias: 'Principal',
-        wpNotifications: false,
-        chatBot: false
-      }
+      3103794656: client
     }
     wrapper = shallowMount(Connection,
       {
         attachTo: '#root',
+        props: {
+          client: client
+        },
         global: {
           plugins: [router, i18n]
         }
@@ -40,14 +45,14 @@ describe('Connection.vue', () => {
   }, 10000)
 
   test('connected var is initialized when mounted', async () => {
-    await wrapper.vm.$nextTick()
+    await nextTick()
     expect(wrapper.find('h4').text()).toMatch('Disconnected')
     expect(wrapper.vm.connected).toBeFalsy()
   })
   
   test('it must throw event when click buttons', async () => {
     wrapper.vm.connected = false
-    await wrapper.vm.$nextTick()
+    await nextTick()
     await wrapper.find('.btn-primary').trigger('click')
     expect(wrapper.vm.qr).toBeNull()
     expect(wrapper.vm.connecting).toBeTruthy()
@@ -55,7 +60,7 @@ describe('Connection.vue', () => {
   
   test('set qr when io emit event qr', async () => {
     socket.emit(WhatsApp.EVENT_QR_CODE, 'fake-qr')
-    await wrapper.vm.$nextTick()
+    await nextTick()
     await waitForExpect(() => {
       expect(wrapper.vm.connecting).toBeFalsy()
       expect(wrapper.vm.qr).toBe('fake-qr')
