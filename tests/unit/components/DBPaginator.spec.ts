61f000dda18f1a3cd5522d38648b4ce2ffe53b16
Alexanderbyrush <alexandrecamilor@gmail.com>
2024-02-03 20:34:42 -0500

    test: Completed all test

--- tests/unit/components/DBPaginator.spec.ts
+++ tests/unit/components/DBPaginator.spec.ts
@@ -77,4 +77,42 @@ describe('DBPaginator.vue', () => {
     await previousPageButton.trigger('click')
     expect(wrapper.find('.page-item.active').text()).toBe('4 (current)')
   })
+
+  it('emits paginatedData event when changing the number of items per page', async () => {
+    const { pagination } = useServicesStore()
+    pagination.perPage = 20
+    createWrapper({
+      totalCount: 100,
+      perPage: 20, 
+      currentPage: 1,
+      cursor: {
+        id: '',
+        created: 0,
+      },
+    })
+    await nextTick()
+    const selectPerPage = wrapper.find('select.form-select')
+    await selectPerPage.setValue(30)
+    expect(pagination.perPage).toBe(30)
+  })
+  
+  it('updates perPage correctly when selecting a different value', async () => {
+    const { pagination } = useServicesStore()
+    pagination.perPage = 20
+    createWrapper({
+      totalCount: 100,
+      perPage: 20, 
+      currentPage: 1,
+      cursor: {
+        id: '',
+        created: 0,
+      },
+    })
+    await nextTick()
+    const selectPerPage = wrapper.find('select.form-select')
+    await selectPerPage.setValue(30)
+    expect(pagination.perPage).toBe(30)
+    await selectPerPage.setValue(20)
+    expect(pagination.perPage).toBe(20)
+  })
 })
