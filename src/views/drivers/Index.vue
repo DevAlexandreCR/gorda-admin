9cdd4f118250718ebbaaa84c33b3ef0fe214e707
Alexander camilo <alexandrecamilor@gmail.com>
2025-05-16 17:33:47 -0500

    Update Index.vue

--- src/views/drivers/Index.vue
+++ src/views/drivers/Index.vue
@@ -118,7 +118,7 @@
 import DateHelper from '@/helpers/DateHelper'
 import Paginator from '@/components/Paginator.vue'
 import {useDriversStore} from '@/services/stores/DriversStore'
-import { useDashboardStore } from '@/services/stores/DashboardStore'
+import { useDashboardStore } from '../../services/stores/dashboardStore'
 import {Field} from 'vee-validate'
 import Driver from '@/models/Driver'
 import {onMounted, ref, Ref, watch, watchEffect} from 'vue'
