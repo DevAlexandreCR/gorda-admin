644718b47d611059a8cd39ef546fa5e15ed8e20a
DevAlexandreCR <devalexandrecr@gmail.com>
2025-05-27 10:30:38 -0500

    fix: update API_BASE and API_PORT for correct FCM messaging endpoint

--- src/services/FcmService.ts
+++ src/services/FcmService.ts
@@ -1,7 +1,8 @@
 import { FCMNotification } from '@/types/FCMNotifications'
 import axios, { AxiosResponse } from 'axios'
 
-const API_BASE = process.env.VUE_APP_GORDA_API_URL
+const API_BASE = process.env.VUE_APP_WP_CLIENT_API_URL
+const API_PORT = process.env.VUE_APP_WP_CLIENT_API_PORT
 
 export interface FcmResponse {
   success: boolean
@@ -11,14 +12,14 @@ export interface FcmResponse {
 
 export default {
   async sendToDriver(driverId: string, message: FCMNotification): Promise<AxiosResponse<FcmResponse>> {
-    return axios.post<FcmResponse>(`${API_BASE}/messages/drivers`, {
+    return axios.post<FcmResponse>(`${API_BASE}:${API_PORT}/messages/drivers`, {
       to: driverId,
       message: message,
     })
   },
 
   async sendToAllDrivers(message: FCMNotification): Promise<AxiosResponse<FcmResponse>> {
-    return axios.post<FcmResponse>(`${API_BASE}/messages/drivers`, {
+    return axios.post<FcmResponse>(`${API_BASE}:${API_PORT}/messages/drivers`, {
       to: null,
       message: message,
     })
