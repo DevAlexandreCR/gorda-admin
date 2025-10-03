1e0a86bc74c7ca2cf8d1524079f260fa8d048c28
DevAlexandreCR <devalexandrecr@gmail.com>
2025-10-03 15:11:51 -0500

    refactor: improve structure of Interactive type by adding button and rows properties

--- src/types/Interactive.ts
+++ src/types/Interactive.ts
@@ -1,23 +1,29 @@
 export type Interactive = {
-    type: 'list' | 'button' | 'product' | 'flow' | 'location_request_message',
-    body?: {
-        text: string
-    },
-    action: {
-        name?: string,
-        buttons?: Array<{
-            type: 'reply',
-            reply?: {
-                id: string,
-                title: string
-            }
-        }>
-    },
-    header?: {
-        type: 'text' | 'image' | 'video' | 'document',
-        text: string
-    }
-    footer?: {
-        text: string
-    }
+  type: 'list' | 'button' | 'product' | 'flow' | 'location_request_message',
+  body?: {
+    text: string
+  },
+  action: {
+    name?: string,
+    button?: string, // Button text for list messages
+    rows?: Array<{
+      id: string,
+      title: string,
+      description?: string
+    }>,
+    buttons?: Array<{
+      type: 'reply',
+      reply?: {
+        id: string,
+        title: string
+      }
+    }>
+  },
+  header?: {
+    type: 'text' | 'image' | 'video' | 'document',
+    text: string
+  }
+  footer?: {
+    text: string
+  }
 }
